c8597fa8377785ef295b6d2653c2c674031423a9
Reinaldo <reinaldo.bustamante026@gmail.com>
2025-05-09 12:40:32 -0400

    chore: configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EditEstudentComponent } from './pages/edit-estudent/edit-estudent.component';
+import { CreateStudentComponent } from './pages/create-student/create-student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'edit', component: EditEstudentComponent},
+    {path: 'new', component: CreateStudentComponent}
+];
---
+++ src/app/pages/edit-estudent/edit-estudent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-estudent',
+  imports: [],
+  templateUrl: './edit-estudent.component.html',
+  styleUrl: './edit-estudent.component.scss'
+})
+export class EditEstudentComponent {
+
+}
