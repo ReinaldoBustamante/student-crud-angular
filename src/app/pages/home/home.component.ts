5273e93e27798bcdd58e55aca73fdf896f681410
Reinaldo <reinaldo.bustamante026@gmail.com>
2025-05-10 14:33:52 -0400

    feat: add home page with components and corresponding styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { StudentsComponent } from './students/students.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, StudentsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
