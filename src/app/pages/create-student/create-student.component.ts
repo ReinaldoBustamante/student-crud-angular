ab01cb1afcd175c05cf8be1f1dff217f1db6a907
Reinaldo <reinaldo.bustamante026@gmail.com>
2025-05-11 18:02:48 -0400

    feat: Add create-student and edit-student pages with his shared form component

--- src/app/pages/create-student/create-student.component.ts
+++ src/app/pages/create-student/create-student.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { StudentFormComponent } from '../../components/student-form/student-form.component';
 
 @Component({
   selector: 'app-create-student',
-  imports: [],
+  imports: [StudentFormComponent],
   templateUrl: './create-student.component.html',
   styleUrl: './create-student.component.scss'
 })
 export class CreateStudentComponent {
+  title: string = 'Nuevo Estudiante'
+  description: string = 'Completa el formulario para registrar un nuevo estudiante'
+  action: string = 'Crear'
 
+  actionFunction(){
+    console.log('crear')
+  }
 }
--- src/app/pages/edit-student/edit-student.component.ts
+++ src/app/pages/edit-student/edit-student.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { StudentFormComponent } from "../../components/student-form/student-form.component";
 
 @Component({
   selector: 'app-edit-student',
-  imports: [],
+  imports: [StudentFormComponent],
   templateUrl: './edit-student.component.html',
   styleUrl: './edit-student.component.scss'
 })
 export class EditStudentComponent {
+  title: string = 'Editar Estudiante'
+  description: string = 'Actualizar la información del estudiante'
+  action: string = 'Actualizar'
 
+  actionFunction(){
+    console.log('editar')
+  }
 }
